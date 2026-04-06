c4c28fe9df38c7026e2b28c41654de5d86a1aeff
Shivam Tyagi <tyagishivamkk512@gmail.com>
2026-04-07 00:44:13 +0530

    OPtimised Version with O(N)

--- DSA_Questions/q20_stock_max_profit.cpp
+++ DSA_Questions/q20_stock_max_profit.cpp
@@ -1,16 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-int maxProfit(vector<int>& prices) {
-        int p=0,max=0;
-        for(int i =0;i<prices.size()-1;i++){
-            for(int j=i+1;j<prices.size();j++){
-                p=prices[j]-prices[i];
-                if(max<p) max=p;
-            }
+    int maxProfit(vector<int>& prices) {
+    int minp = prices[0];
+    int profit = 0;
+
+    for(int i = 0; i < prices.size(); i++){
+        if(prices[i] < minp){
+            minp = prices[i];
         }
-        if(max<0) max=0;
-        return max;
+        else{
+            profit = max(profit, prices[i] - minp);
+        }
     }
+    return profit;
+}
 
 int main(){
     vector <int> nums = {7,1,5,3,6,4};
