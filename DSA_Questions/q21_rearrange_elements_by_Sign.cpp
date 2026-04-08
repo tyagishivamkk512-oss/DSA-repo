1c5db8e0dfdb6818c615df4bd29373dff5cd3b55
Shivam Tyagi <tyagishivamkk512@gmail.com>
2026-04-08 18:35:47 +0530

    More optimised approach

--- DSA_Questions/q21_rearrange_elements_by_Sign.cpp
+++ DSA_Questions/q21_rearrange_elements_by_Sign.cpp
@@ -1,25 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-    vector <int> rearrangeArray(vector<int>& nums) {
-    int i =0,j=0;
-    int n = nums.size();
-    vector <int> v;
-    while(i<n && j<n){
-        while(nums[i]<0 && i<n){
-            i++;
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        int i=0,j=1;
+        vector<int> v(n,0);
+        for(int k=0;k<n;k++){
+            if(nums[k]<0){
+                v[j]=nums[k];
+                j+=2;
+            }
+            else{
+                v[i]=nums[k];
+                i+=2;
+            }
         }
-        if(i<n){
-            v.emplace_back(nums[i]);
-        }
-        while(nums[j]>0&&j<n){
-            j++;
-        }
-        if(j<n){
-            v.emplace_back(nums[j]);
-        }
+        return v;
     }
-    return v;
-}
 
 int main(){
     vector <int> nums = {3,1,-2,-5,2,-4};
