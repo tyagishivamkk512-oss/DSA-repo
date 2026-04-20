2f4bcb93dcf3dbfaf322d1e4b05312cf16a6b473
Shivam Tyagi <tyagishivamkk512@gmail.com>
2026-04-21 00:26:58 +0530

    A better approach in time complexity but requires extra space

--- DSA_Questions/q25_set_matrix_zeroes.cpp
+++ DSA_Questions/q25_set_matrix_zeroes.cpp
@@ -1,29 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-void mark_row(int i,int m,vector<vector<int>>& v){
-        for(int k=0;k<m;k++){
-            if(v[i][k]!=0) v[i][k]=-1;
-        }
-    }
-    void mark_column(int j,int n,vector<vector<int>>& v){
-        for(int k=0;k<n;k++){
-            if(v[k][j]!=0) v[k][j]=-1;
-        }
-    }
-    void setZeroes(vector<vector<int>>& v) {
+void setZeroes(vector<vector<int>>& v) {
         int n = v.size();
         int m = v[0].size();
+        vector<int> row(n,0);
+        vector<int> column(m,0);
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(v[i][j]==0){
-                    mark_row(i,m,v);
-                    mark_column(j,n,v);
+                    row[i]=1;
+                    column[j]=1;
                 }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(v[i][j]==-1) v[i][j]=0;
+                if(row[i]==1||column[j]==1) v[i][j]=0;
             }
         }
     }
