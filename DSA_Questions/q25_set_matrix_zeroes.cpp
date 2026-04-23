cf0c6edea217507d8d1ad396e40311460dec7a55
Shivam Tyagi <tyagishivamkk512@gmail.com>
2026-04-24 01:18:10 +0530

    most optimal solution with space compexity in order of N

--- DSA_Questions/q25_set_matrix_zeroes.cpp
+++ DSA_Questions/q25_set_matrix_zeroes.cpp
@@ -1,24 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
-void setZeroes(vector<vector<int>>& v) {
-        int n = v.size();
-        int m = v[0].size();
-        vector<int> row(n,0);
-        vector<int> column(m,0);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(v[i][j]==0){
-                    row[i]=1;
-                    column[j]=1;
-                }
-            }
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(row[i]==1||column[j]==1) v[i][j]=0;
+void setZeroes(vector<vector<int>>& nums) {
+    int n = nums.size();
+    int m = nums[0].size();
+    int col0=1;
+    for(int i =0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(nums[i][j]==0){
+                nums[i][0]==0;
+                if(j!=0) nums[0][j]==0;
+                else col0=0;
             }
         }
     }
+    for(int i =1;i<n;i++){
+        for(int j=1;j<m;j++){
+            if(nums[i][j]!=0){
+                if(nums[i][0]==0||nums[0][j]==0){
+                    nums[i][j]==0;
+                }
+            }
+        }
+    }
+    if(nums[0][0]==0){
+        for(int i =0;i<m;i++){
+            nums[0][i]=0;
+        }
+    }
+    if(col0==0){
+        for(int i=0;i<n;i++){
+            nums[i][0]=0;
+        }
+    }
+}
 
 int main() {
     vector<vector<int>> v = {{1,1,0},{0,1,1},{1,1,1}};
