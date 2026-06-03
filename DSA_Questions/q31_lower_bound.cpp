3282656fa688ac9aa9999193f5887e6231967a93
Shivam Tyagi <tyagishivamkk512@gmail.com>
2026-06-03 08:55:33 +0530

    more crisp approach to solve that problem

--- DSA_Questions/q31_lower_bound.cpp
+++ DSA_Questions/q31_lower_bound.cpp
@@ -2,45 +2,23 @@
 using namespace std;        
 
 int lowerBound(vector<int>& nums, int target) {
-        int low = 0;
-        int high = nums.size()-1;
-        int x = nums.size();
-        int n = nums.size();
-        
-        
-        while(low <= high){
-            int mid = low + (high-low)/2;
+    int low = 0;
+    int high = nums.size() - 1;
+    int ans = nums.size();
 
-            if(nums[mid] == target){
-                x = mid;
-                high = mid - 1;
-            }
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
 
-            else if(nums[mid] < target){
-                if(mid != n - 1){
-                    if(nums[mid + 1] > target ){
-                        x = mid +1;
-                        return x;
-                    }
-                    else low = mid +1;
-                }
-                else return n;
-            }
-
-            else{
-                if(mid != 0){
-                    if(nums[mid - 1] < target){
-                        x = mid ;
-                        return x;
-                    }
-                    else high = mid - 1;
-                }
-                else return mid;
-            }
+        if (nums[mid] >= target) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
         }
-        return x;
-        
     }
+
+    return ans;
+}
 
 int main(){
     vector <int> nums = {0,1,2,3,5,6,9,11,17,19};
