cf12798c896ca721cd5a45c745c6ea356868c754
Shivam Tyagi <tyagishivamkk512@gmail.com>
2026-06-04 13:14:58 +0530

    brute force approach

---
+++ DSA_Questions/q34_search_insert_position.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;        
+
+int searchInsert(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+        int ans = 0;
+
+        if(nums[high] < target) return high + 1;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] == target) return mid;
+
+            else if(nums[mid] > target){
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+
+        }
+        return ans;
+    }
+
+int main(){
+    vector <int> nums = {0,1,2,3,5,6,9,11,17,19};
+    searchInsert(nums,9);
+}
