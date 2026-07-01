2f44720b2657325050787035f7d5a3de11f6c7cd
Shivam Tyagi <tyagishivamkk512@gmail.com>
2026-07-02 00:23:33 +0530

    optimal approach

--- DSA_Questions/q41_3_sum.cpp
+++ DSA_Questions/q41_3_sum.cpp
@@ -1,22 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;        
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
-        set<vector<int>> temp;
         int n = nums.size();
-        for(int i = 0; i < n; i++){
-            unordered_set <int> hash;
-            for(int j = i+1; j < n; j++ ){
-                int rem = -(nums[i]+nums[j]);
-                if(hash.find(rem)!=hash.end()){
-                    vector <int> a = {nums[i],nums[j],rem};
-                    sort(a.begin(),a.end());
-                    temp.insert(a);
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for(int i = 0;i <n -2; i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int j = i+1 , k = n - 1;
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum > 0) k--;
+                else if(sum < 0) j++;
+                else{
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    while(j< k && nums[j] == nums[j-1]) j++;
+                    while(j< k && nums[k] == nums[k+1]) k--;
                 }
-                hash.insert(nums[j]);
             }
         }
-        return vector<vector<int>>(temp.begin(), temp.end());
+        return ans;
     }
 
 
